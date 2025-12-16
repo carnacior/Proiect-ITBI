e7817d4814e712f132dbe36f05f9896283854be2
Olteanu Luca <lucanaluca3@gmail.com>
2025-12-16 15:49:27 +0200

    prima versiune, am scris ideea de baza pnetru creearea directoarelor cu fisierul procs ce va contine mesajul returnat de comanda ps

--- UserFS.sh
+++ UserFS.sh
@@ -1,1 +1,13 @@
-ps
+#!/bin/bash
+
+users=$(who | awk '{print $1}')
+for user in users: do
+	if[ ! find . -name "$user" -type d]; then
+		mkdir "$user"
+		cd "$user"
+		touch procs
+		ps aux > procs
+	else
+		cd "$user"
+		ps aux > procs
+
