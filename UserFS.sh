3e3a490db8bbecab96d87d3d6af84ffab76e90ad
Olteanu Luca <lucanaluca3@gmail.com>
2026-01-02 22:00:24 +0200

    Versiunea finala

--- UserFS.sh
+++ UserFS.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-users=$(who | awk '{print $1}')
-for user in users: do
-	if[ ! find . -name "$user" -type d]; then
-		mkdir "$user"
-		cd "$user"
-		touch procs
-		ps aux > procs
-	else
-		cd "$user"
-		ps aux > procs
+root_dir="/home/lucaa303/UserFS/Root"
 
+while true; do
+	active_users=$(who | awk '{print $1}')
+	for user in $active_users; do
+		if [ ! -d "$root_dir/$user" ]; then
+			mkdir "$root_dir/$user"
+			touch "$root_dir/$user/procs"
+		fi
+		ps aux > "$root_dir/$user/procs"
+		rmdir "$root_dir/$user/lastlogin"
+	done
+	for user_dir in "$root_dir"/*/; do
+		userul=$(basename "$user_dir")
+		if ! echo "$active_users" | grep -q "^$userul$"; then
+			touch "$root_dir/$user/lastlogin"
+			: > "$user_dir/procs"
+        		date '+%Y-%m-%d %H:%M:%S' > "$user_dir/lastlogin"
+		fi
+	done
+	sleep 30
+done
