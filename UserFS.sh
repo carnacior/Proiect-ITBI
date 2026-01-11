2778f5c5678ad126efb75def5ca01dd15d3837a9
Olteanu Luca <lucanaluca3@gmail.com>
2026-01-11 23:28:53 +0200

    Aceasta este versiunea finala, am rezolvat cateva buguri minore si am imbunatatit comada care afiseaza procesele pentru a fi specifice userului activ.

--- UserFS.sh
+++ UserFS.sh
@@ -3,22 +3,23 @@
 root_dir="/home/lucaa303/UserFS/Root"
 
 while true; do
-	active_users=$(who | awk '{print $1}')
-	for user in $active_users; do
-		if [ ! -d "$root_dir/$user" ]; then
-			mkdir "$root_dir/$user"
-			touch "$root_dir/$user/procs"
-		fi
-		ps aux > "$root_dir/$user/procs"
-		rmdir "$root_dir/$user/lastlogin"
-	done
-	for user_dir in "$root_dir"/*/; do
-		userul=$(basename "$user_dir")
-		if ! echo "$active_users" | grep -q "^$userul$"; then
-			touch "$root_dir/$user/lastlogin"
-			: > "$user_dir/procs"
-        		date '+%Y-%m-%d %H:%M:%S' > "$user_dir/lastlogin"
-		fi
-	done
-	sleep 30
+        active_users=$(who | awk '{print $1}')
+        for user in $active_users; do
+                if [ ! -d "$root_dir/$user" ]; then
+                        mkdir "$root_dir/$user"
+                        touch "$root_dir/$user/procs"
+                fi
+                ps -u "$user" -o pid,comm  > "$root_dir/$user/procs"
+                rm "$root_dir/$user/lastlogin"
+        done
+        for user_dir in "$root_dir"/*/; do
+                userul=$(basename "$user_dir")
+                if ! echo "$active_users" | grep -q "^$userul$"; then
+                        touch "$root_dir/$user/lastlogin"
+                        : > "$user_dir/procs"
+                        date '+%Y-%m-%d %H:%M:%S' > "$user_dir/lastlogin"
+                fi
+        done
+        sleep 30
 done
+
